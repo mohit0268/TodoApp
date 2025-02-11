50e7a27575ba5a1a042a2e3f610c8e7f90aa9d20
Mohit Purohit <mohitpurohit020@gmail.com>
2025-02-11 19:02:11 +0530

    Getting started with the project

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <Text>Getting started....</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
   },
 });
