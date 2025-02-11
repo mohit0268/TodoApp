6e983feb5ab763a39ef035c321cdbd38e68c13d8
Mohit Purohit <mohitpurohit020@gmail.com>
2025-02-11 19:07:47 +0530

    added input field and button

--- App.js
+++ App.js
@@ -1,18 +1,64 @@
-import { StyleSheet, View, Text } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Button,
+  View,
+  Text,
+  TextInput,
+  FlatList
+} from "react-native";
+
 
 export default function App() {
+  const [inputText, setInputText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function textChangeHandler(enteredText) {
+    setInputText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals((prev) => [...prev, {
+      text:inputText,
+      key:Math.random().toString()
+    }]);
+  }
   return (
     <View style={styles.appContainer}>
-      <Text>Getting started....</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Search for the item"
+          style={styles.textInputContainer}
+          onChangeText={textChangeHandler}
+        ></TextInput>
+        <Button
+          title="Click"
+          accessibilityLabel="Learn more about by clicking it"
+          onPress={addGoalHandler}
+        />
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100%",
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 15,
   },
+  inputContainer: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  textInputContainer: {
+    borderWidth: 1,
+    borderBlockColor: "black",
+    width: "80%",
+  }
 });
